e21fd7f080c68568db382bd62302294f2b7fd84b
wa101 <wadhah.mahroug15@gmail.com>
2022-10-25 19:20:15 +0100

    config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,7 +30,7 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout title={`Hello from ${siteConfig.title}`} description="">
+    <Layout title={`Home`} description="">
       <HomepageHeader />
     </Layout>
   );

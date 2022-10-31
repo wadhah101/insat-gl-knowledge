4f8abc5416272b16fe2999a44b292fbf397fc8df
wadhah mahrouk <wadhah.mahroug15@gmail.com>
2022-10-31 19:09:26 +0100

    gl5

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,15 +35,13 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/wadhah101/insat-gl-knowledge",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/wadhah101/insat-gl-knowledge",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),

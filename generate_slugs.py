81f264ccfbc96056fab33878944303a2938e2d53
wadhah mahrouk <wadhah.mahroug15@gmail.com>
2022-11-04 22:05:23 +0100

    format script

--- generate_slugs.py
+++ generate_slugs.py
@@ -1,17 +1,17 @@
 from pathlib import Path
 import re
 import os
-result = Path("docs").glob('**/*.md')
 
-pattern = r'---\n([\S\s]*)---'
+result = Path("docs").glob("**/*.md")
+
+pattern = r"---\n([\S\s]*)---"
 
 for i in result:
-  with open(i) as f:
-    content = f.read()
-    contains_meta = re.search(pattern, content)
-  if not contains_meta:
-    slug = i.relative_to("docs").as_posix().lower().replace(' ', '-').rstrip(".md")
-    slugg_meta = f"---\nslug: /{slug}\n---"""
-    with open(i , "w") as f:
-      f.write(slugg_meta + "\n\n" +content)
-
+    with open(i) as f:
+        content = f.read()
+        contains_meta = re.search(pattern, content)
+    if not contains_meta:
+        slug = i.relative_to("docs").as_posix().lower().replace(" ", "-").rstrip(".md")
+        slugg_meta = f"---\nslug: /{slug}\n---" ""
+        with open(i, "w") as f:
+            f.write(slugg_meta + "\n\n" + content)

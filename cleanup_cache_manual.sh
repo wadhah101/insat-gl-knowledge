b59fc8115a96ebb342a117c15d408b4e1dcd8efc
wadhah mahrouk <wadhah.mahroug15@gmail.com>
2022-11-13 19:49:05 +0100

    cleanup

--- cleanup_cache_manual.sh
+++ cleanup_cache_manual.sh
@@ -4,15 +4,15 @@
 
 mapfile -t BRANCHES < <(git ls-remote --heads | cut -d$'\t' -f2)
 printf "branches :  %s\n" "${BRANCHES[@]}"
+echo
 
 for BRANCH in "${BRANCHES[@]}"; do
   mapfile -t CACHES < <(gh actions-cache list -B "$BRANCH" --limit 100 | grep Linux-webpack | cut -d$'\t' -f1)
-  printf "%s : %s" "$BRANCH" "${#CACHES[@]}"
+  printf "%s : %s\n" "$BRANCH" "${#CACHES[@]}"
   if [ "${#CACHES[@]}" -lt 2 ]; then
-    echo "no cache to delete"
+    printf "no cache to delete\n\n"
   else
-    echo "deleting irrelevant cache"
+    printf "deleting irrelevant cache\n\n"
     parallel -j 5 gh actions-cache delete --confirm ::: "${CACHES[@]:1}"
   fi
-  echo
 done

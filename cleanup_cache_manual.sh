a1196ec761e05e0488ef9552dca7ff0c501b744b
wadhah mahrouk <wadhah.mahroug15@gmail.com>
2022-11-13 19:36:19 +0100

    fix

--- cleanup_cache_manual.sh
+++ cleanup_cache_manual.sh
@@ -3,17 +3,18 @@
 # Simple script to cleanup useless github action cache periodically
 
 BRANCHES=($(git ls-remote --heads | cut -d$'\t' -f2))
-echo branches : ${BRANCHES[@]}
+echo "branches : ${BRANCHES[@]}\n"
 
 for BRANCH in "${BRANCHES[@]}"
 do
   CACHES=($(gh actions-cache list -B $BRANCH --limit 100 | grep Linux-webpack |  cut -d$'\t' -f1))
   echo $BRANCH : "${#CACHES[@]}"
   if [ "${#CACHES[@]}" -lt 2 ]; then
-    echo "$BRANCH : no cache to delete"
+    echo "no cache to delete"
   else
-    echo "$BRANCH : deleting irrelevant cache"
+    echo "deleting irrelevant cache"
     parallel -j 5 gh actions-cache delete --confirm ::: ${CACHES[@]:1}
   fi
+  echo
 done
 

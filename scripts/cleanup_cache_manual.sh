a32248280d3edf995dbcbb0813d9f979252b5b26
wadhah mahrouk <wadhah.mahroug15@gmail.com>
2022-11-14 12:27:42 +0100

    scoped cleanup

--- scripts/cleanup_cache_manual.sh
+++ scripts/cleanup_cache_manual.sh
@@ -3,18 +3,23 @@
 # Simple script to cleanup useless github action cache periodically
 # requires bash > 4.4
 
-mapfile -t BRANCHES < <(gh actions-cache list | grep -E "Linux-webpack.*ref" | cut -d$'\t' -f3 | uniq)
+if [ -z "$GITHUB_REF" ]; then
+  mapfile -t BRANCHES < <(gh actions-cache list | grep -E "Linux-webpack.*ref" | cut -d$'\t' -f3 | uniq)
+else
+  BRANCHES=("$GITHUB_REF")
+fi
+
 printf "branches :  %s\n" "${BRANCHES[@]}"
 echo
 
 for BRANCH in "${BRANCHES[@]}"; do
   # useless calls to gh api, if we group "gh actions-cache list" by branch name and then filter the array from the latest element, too much work, this script works fast enough
   mapfile -t CACHES < <(gh actions-cache list -B "$BRANCH" --limit 100 | grep Linux-webpack | cut -d$'\t' -f1)
-  printf "%s : %s\n" "$BRANCH" "${#CACHES[@]}"
+  printf "\n%s : %s\n" "$BRANCH" "${#CACHES[@]}"
   if [ "${#CACHES[@]}" -lt 2 ]; then
-    printf "no cache to delete\n\n"
+    printf "no cache to delete\n"
   else
-    printf "deleting irrelevant cache\n\n"
+    printf "deleting irrelevant cache\n"
     parallel -j 5 gh actions-cache delete --confirm ::: "${CACHES[@]:1}"
   fi
 done

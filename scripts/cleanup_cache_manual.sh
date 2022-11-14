bc52aafdd99f680e06170ae95691bbd2a11011d6
wadhah mahrouk <wadhah.mahroug15@gmail.com>
2022-11-14 12:21:10 +0100

    add cleanup on build workflow

--- scripts/cleanup_cache_manual.sh
+++ scripts/cleanup_cache_manual.sh
@@ -3,11 +3,12 @@
 # Simple script to cleanup useless github action cache periodically
 # requires bash > 4.4
 
-mapfile -t BRANCHES < <(git ls-remote --heads | cut -d$'\t' -f2)
+mapfile -t BRANCHES < <(gh actions-cache list | grep -E "Linux-webpack.*ref" | cut -d$'\t' -f3 | uniq)
 printf "branches :  %s\n" "${BRANCHES[@]}"
 echo
 
 for BRANCH in "${BRANCHES[@]}"; do
+  # useless calls to gh api, if we group "gh actions-cache list" by branch name and then filter the array from the latest element, too much work, this script works fast enough
   mapfile -t CACHES < <(gh actions-cache list -B "$BRANCH" --limit 100 | grep Linux-webpack | cut -d$'\t' -f1)
   printf "%s : %s\n" "$BRANCH" "${#CACHES[@]}"
   if [ "${#CACHES[@]}" -lt 2 ]; then

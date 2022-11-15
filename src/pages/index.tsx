214b35954573c6dcd45e5b863e84cf886c6e97b2
wadhah mahrouk <wadhah.mahroug15@gmail.com>
2022-11-15 10:57:36 +0100

    contributor filter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,8 +33,8 @@ interface ContributorListProps {
 const ContributorList: React.FC<ContributorListProps> = ({ contributors }) => {
   return (
     <div>
-      <h2 className="text-3xl text-center pb-3"> CONTRIBUTORS </h2>
-      <div className="grid gap-6 grid-cols-5">
+      <h2 className="pb-3 text-3xl text-center"> CONTRIBUTORS </h2>
+      <div className="grid grid-cols-5 gap-6">
         {contributors.map((e) => (
           <div key={e.id}>
             <a
@@ -44,7 +44,7 @@ const ContributorList: React.FC<ContributorListProps> = ({ contributors }) => {
               href={e.html_url}
             >
               <div className="flex flex-col">
-                <img className="h-24 rounded-full h-24" src={e.avatar_url} />
+                <img className="h-24 rounded-full" src={e.avatar_url} />
               </div>
             </a>
           </div>
@@ -54,15 +54,15 @@ const ContributorList: React.FC<ContributorListProps> = ({ contributors }) => {
   );
 };
 
-const filteredContributors = contributors
-  .filter((e) => e.login !== "ImgBotApp")
-  .sort((e1, e2) => e2.contributions - e2.contributions);
+const filteredContributors = contributors.filter(
+  (e) => !["ImgBotApp", "pre-commit-ci[bot]"].find((x) => x === e.login)
+);
 
 export default function Home(): JSX.Element {
   return (
     <Layout title={`Home`} description="">
       <HomepageHeader />
-      <div className="py-20 grid place-items-center ">
+      <div className="grid py-20 place-items-center">
         <ContributorList contributors={filteredContributors} />
       </div>
     </Layout>
